083f85d1a889a64dd7f71270a8c6e8df5ac2952e
Metal "Pizza" Cat <36876492+MetalPizzaCat@users.noreply.github.com>
2023-04-08 17:58:19 +1000

    update visuals for nodes

--- Script/ExportImageGenerator.cs
+++ Script/ExportImageGenerator.cs
@@ -8,31 +8,62 @@ using System.Linq;
 public static class ExportImageGenerator
 {
 
+    public static int LineWidth = 3;
+    public static Vector2 BlockSize = new Vector2(64, 128);
+    public static Vector2 CanvasSafeZoneTop = new Vector2(100, 100);
+    public static Vector2 CanvasSafeZoneBottom = new Vector2(256, 256);
+
     public static string? GetXml(List<LogicNode> nodes, List<ConnectionWire> wires)
     {
-        float minX = Math.Abs(Math.Min(nodes.MinBy(x => x.GlobalPosition.X)?.GlobalPosition.X ?? 0, 0)) + 100;
-        float minY = Math.Abs(Math.Min(nodes.MinBy(x => x.GlobalPosition.Y)?.GlobalPosition.Y ?? 0, 0)) + 100;
+        float minX = Math.Abs(Math.Min(nodes.MinBy(x => x.GlobalPosition.X)?.GlobalPosition.X ?? 0, 0)) + CanvasSafeZoneTop.X;
+        float minY = Math.Abs(Math.Min(nodes.MinBy(x => x.GlobalPosition.Y)?.GlobalPosition.Y ?? 0, 0)) + CanvasSafeZoneTop.Y;
         GD.Print($"MinX {minX} MinY {minY}");
         XElement root = new XElement("svg");
-        root.SetAttributeValue("width", (nodes.MaxBy(x => x.GlobalPosition.X)?.GlobalPosition.X ?? 0) - (nodes.MinBy(x => x.GlobalPosition.X)?.GlobalPosition.X ?? 0) + 256);
-        root.SetAttributeValue("height", (nodes.MaxBy(x => x.GlobalPosition.Y)?.GlobalPosition.Y ?? 0) - (nodes.MinBy(x => x.GlobalPosition.Y)?.GlobalPosition.Y ?? 0) + 256);
+        root.SetAttributeValue("width", (nodes.MaxBy(x => x.GlobalPosition.X)?.GlobalPosition.X ?? 0) - (nodes.MinBy(x => x.GlobalPosition.X)?.GlobalPosition.X ?? 0) + CanvasSafeZoneBottom.X);
+        root.SetAttributeValue("height", (nodes.MaxBy(x => x.GlobalPosition.Y)?.GlobalPosition.Y ?? 0) - (nodes.MinBy(x => x.GlobalPosition.Y)?.GlobalPosition.Y ?? 0) + CanvasSafeZoneBottom.Y);
         GD.Print($"MaxX: {(nodes.MaxBy(x => x.GlobalPosition.X)?.GlobalPosition.X ?? 0)} MaxY: {(nodes.MaxBy(x => x.GlobalPosition.Y)?.GlobalPosition.Y ?? 0)}; MinX: {(nodes.MinBy(x => x.GlobalPosition.X)?.GlobalPosition.X ?? 0)} MinY: {(nodes.MinBy(x => x.GlobalPosition.Y)?.GlobalPosition.Y ?? 0)}");
         foreach (LogicNode node in nodes)
         {
             XElement rect = new XElement("rect");
-            rect.SetAttributeValue("width", 128);
-            rect.SetAttributeValue("height", 128);
+            foreach (Connector conn in node.Inputs)
+            {
+                Vector2 pos = conn.GlobalPosition + new Vector2(minX, minY);// + new Vector2(32, 64);
+                XElement line = new XElement("line");
+                line.SetAttributeValue("x1", pos.X - 32);
+                line.SetAttributeValue("x2", pos.X + 16);
+                line.SetAttributeValue("y1", pos.Y);
+                line.SetAttributeValue("y2", pos.Y);
+                line.SetAttributeValue("stroke-width", LineWidth);
+                line.SetAttributeValue("stroke", "black");
+                root.Add(line);
+            }
+
+            foreach (Connector conn in node.Outputs)
+            {
+                Vector2 pos = conn.GlobalPosition + new Vector2(minX, minY);//+ new Vector2(32, 64); ;
+                XElement line = new XElement("line");
+                line.SetAttributeValue("x1", pos.X - 16);
+                line.SetAttributeValue("x2", pos.X + 32);
+                line.SetAttributeValue("y1", pos.Y);
+                line.SetAttributeValue("y2", pos.Y);
+                line.SetAttributeValue("stroke-width", LineWidth);
+                line.SetAttributeValue("stroke", "black");
+                root.Add(line);
+            }
+
+            rect.SetAttributeValue("width", BlockSize.X);
+            rect.SetAttributeValue("height", BlockSize.Y);
             rect.SetAttributeValue("fill", "white");
             rect.SetAttributeValue("stroke-width", 5);
             rect.SetAttributeValue("stroke", "black");
-            rect.SetAttributeValue("x", node.GlobalPosition.X + minX - 128 / 2);
-            rect.SetAttributeValue("y", node.GlobalPosition.Y + minY - 128 / 2);
+            rect.SetAttributeValue("x", node.GlobalPosition.X + minX - BlockSize.X / 2f);
+            rect.SetAttributeValue("y", node.GlobalPosition.Y + minY - BlockSize.Y / 2f);
             root.Add(rect);
         }
         foreach (ConnectionWire wire in wires)
         {
             XElement line = new XElement("polyline");
-            line.SetAttributeValue("stroke-width", 5);
+            line.SetAttributeValue("stroke-width", LineWidth);
             line.SetAttributeValue("stroke", "black");
             line.SetAttributeValue("fill", "none");
             string points = string.Empty;
